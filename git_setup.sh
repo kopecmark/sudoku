03bf519ae06a74f9dff5ee965a77f7d9a88b5b28
Asher King Abramson <akingabramson@appacademy.io>
2016-01-07 16:17:13 -0800

    track branches correctly in setup script

--- git_setup.sh
+++ git_setup.sh
@@ -1,2 +1,11 @@
 for remote in `git branch -r | egrep -v master`; do git branch --track $remote; done
 git fetch --all
+BRANCHES=`git branch | egrep -v master | cut -f 2 -d "/"`
+echo $BRANCHES
+
+for branch in $BRANCHES; do
+  git checkout $branch;
+done
+
+git checkout master;
+
