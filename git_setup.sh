2e600e08bd6d71c9d6ff076ed138a7102405c40a
Asher King Abramson <akingabramson@appacademy.io>
2016-01-07 16:24:05 -0800

    remove echo from git setup

--- git_setup.sh
+++ git_setup.sh
@@ -1,7 +1,6 @@
 for remote in `git branch -r | egrep -v master`; do git branch --track $remote; done
 git fetch --all
 BRANCHES=`git branch | egrep -v master | cut -f 2 -d "/"`
-echo $BRANCHES
 
 for branch in $BRANCHES; do
   git checkout $branch;
